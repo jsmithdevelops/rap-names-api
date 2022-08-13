d5ea5bdc1c4a360818d3bc84b2c9f06c22c1d26a
jsmith2589 <65322603+jsmith2589@users.noreply.github.com>
2022-08-12 23:43:02 -0400

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
 const PORT = 8000
+const cors = require("cors")
 
+app.use(cors())
 const rappers = {
   "jonny be good": {
     birthName: "Jonathan Smith",
